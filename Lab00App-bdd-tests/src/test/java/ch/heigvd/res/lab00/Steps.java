c979deaaf702fe7d47490b569b13eaa3730de616
miiicmueller <michael.muller@he-arc.ch>
2015-03-16 13:32:19 +0100

    Cucumber glue code modification

--- Lab00App-bdd-tests/src/test/java/ch/heigvd/res/lab00/Steps.java
+++ Lab00App-bdd-tests/src/test/java/ch/heigvd/res/lab00/Steps.java
@@ -1,6 +1,5 @@
 package ch.heigvd.res.lab00;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -39,5 +38,31 @@ public class Steps {
     assertNotEquals("", music);
   }
 
+  @Given("^there is an orchestra$")
+  public void there_is_an_orchestra() throws Throwable {
+    orchestra = new ArrayList<>();
+  }
+
+  @Given("^there is (\\d+) flute player in the the orchestra$")
+  public void there_is_flute_player_in_the_the_orchestra(int numberOfFlutes) throws Throwable {
+    for (int i = 0; i < numberOfFlutes; i++) {
+      orchestra.add(new Flute());
+    }
+  }
+
+  @When("^I ask the orchestra to play$")
+  public void i_ask_the_orchestra_to_play() throws Throwable {
+    i_ask_them_to_play();
+  }
+
+  @Then("^I hear the sound of flute$")
+  public void i_hear_the_sound_of_flute() throws Throwable {
+    assertNotEquals(-1, music.indexOf("piout"));
+  }
+
+  @Then("^I don't hear the sound of trumpet$")
+  public void i_don_t_hear_the_sound_of_trumpet() throws Throwable {
+    assertEquals(-1, music.indexOf("pouet"));
+  }
 
 }
--- Lab00App-bdd-tests/src/test/resources/ch/heigvd/res/lab00/orchestra.feature
+++ Lab00App-bdd-tests/src/test/resources/ch/heigvd/res/lab00/orchestra.feature
@@ -4,3 +4,10 @@ Feature: Orchestra
     Given there is an orchestra available
     When I ask them to play
     Then I hear some music
+
+  Scenario: listen to a solo flute player
+    Given there is an orchestra
+    And there is 1 flute player in the the orchestra
+    When I ask the orchestra to play
+    Then I hear the sound of flute
+    And I don't hear the sound of trumpet
